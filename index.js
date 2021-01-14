7662830070ec62a0915145de7e87853a99dd1575
Jacob Marthaller <https://github.com/jmarthaller>
2021-01-13 19:41:09 -0500

    commit tree 6bdfe4f27576d84274f14899b9616ea6046bb7cb

---
+++ index.js
@@ -0,0 +1,24 @@
+
+const divide = () => 2000/100
+
+
+// const makeStudent = (name, grade) => ({ 
+//     name: name, 
+//     grade: grade 
+// })
+
+
+
+
+
+const square = num => {
+    return num**2
+}
+
+
+const add = (a, b) => {
+    return a + b
+}
+
+
+// divide(2000, 100)
